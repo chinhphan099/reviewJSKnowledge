d7c101aad6c6aaee94ea5374db9b32d83dcb23e2
Chinh Phan <chinhphan099@gmail.com>
2023-03-08 21:05:14 -0500

    ES6 Module

---
+++ ES6_Module/app.js
@@ -0,0 +1,6 @@
+import React from './react.js' // Có thể đặt tên khác, code tự nhận là giá trị mặc định của export default: import ReactParam from './react.js'
+import { fn, value as valueParam } from './react.js' // Sử dụng as để đổi tên
+
+console.log(React)
+console.log(valueParam)
+fn()
---
+++ ES6_Module/react.js
@@ -0,0 +1,4 @@
+const React = 'React'
+export const fn = () => console.log('function')
+export const value = 100
+export default React
